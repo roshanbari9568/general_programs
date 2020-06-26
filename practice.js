681d49222119e6cc5f6647691217ab0673045e94
CHANDRAROSHAN S BARI <i.m.roshanbari68@gmail.com>
2020-06-26 14:29:37 +0530

    regex.me

--- practice.js
+++ practice.js
@@ -1,20 +1,20 @@
-// let str = "12345.00";
-// str = str.slice(0, -2); 
-// console.log(str);
+let str = "12345.00";
+str = str.slice(0, -2); 
+console.log(str);
 
-// var path = "menahi bin tha\'il-1589541512222";
-// var path2 = path.replace(/\\|\'/g, "");  //Here i m removing \ and ' from the string
-// console.log(path2);
+var path = "menahi bin tha\'il-123456";
+var path2 = path.replace(/\\|\'/g, "");  //Here i m removing \ and ' from the string
+console.log(path2);
 
 
-// var addresses = `1600 1/2 Main St
-// 1600 3/4 Main Street
-// 1600.5 Main St
-// 1600.75 3rd Street`
+var addresses = `1600 1/2 Main St
+1600 3/4 Main Street
+1600.5 Main St
+1600.75 3rd Street`
 
-// console.log(
-// addresses.replace(/ ?\d\/\d?|\.\d{1,}/g,"")
-// )
+console.log(
+addresses.replace(/ ?\d\/\d?|\.\d{1,}/g,"")
+)
 
 
 
